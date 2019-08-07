7f1f827935ac42e9de234400a1ce671835cfc120
Audrey Eschright <audrey@hashicorp.com>
2019-08-07 12:02:14 -0700

    Test GetSession functions

--- session_test.go
+++ session_test.go
@@ -49,6 +49,82 @@ func TestGetSessionOptions(t *testing.T) {
 	}
 }
 
+func TestGetSessionWithBlankConfig(t *testing.T) {
+	oldEnv := initSessionTestEnv()
+	defer awstesting.PopEnv(oldEnv)
+
+	_, err := GetSession(&Config{})
+	if err == nil {
+		t.Fatal("GetSession(&Config{}) with an empty config should result in an error")
+	}
+}
+
+func TestGetSessionWithCreds(t *testing.T) {
+	oldEnv := initSessionTestEnv()
+	defer awstesting.PopEnv(oldEnv)
+
+	sess, err := GetSession(&Config{
+		AccessKey:            "MockAccessKey",
+		SecretKey:            "MockSecretKey",
+		SkipCredsValidation:  true,
+		SkipMetadataApiCheck: true,
+		MaxRetries:           1,
+		UserAgentProducts:    []*UserAgentProduct{{}},
+	})
+	if err != nil {
+		t.Fatalf("GetSession(&Config{...}) should return a valid session, but got the error %s", err)
+	}
+
+	if sess == nil {
+		t.Error("GetSession(...) resulted in a nil session")
+	}
+}
+
+func TestGetSessionWithAccountIDAndPartition(t *testing.T) {
+	oldEnv := initSessionTestEnv()
+	defer awstesting.PopEnv(oldEnv)
+
+	ts := MockAwsApiServer("STS", []*MockEndpoint{
+		{
+			Request:  &MockRequest{"POST", "/", "Action=GetCallerIdentity&Version=2011-06-15"},
+			Response: &MockResponse{200, stsResponse_GetCallerIdentity_valid, "text/xml"},
+		},
+	})
+	defer ts.Close()
+
+	tt := []struct {
+		desc              string
+		config            *Config
+		expectedAcctID    string
+		expectedPartition string
+	}{
+		{"StandardProvider_Config", &Config{AccessKey: "MockAccessKey", SecretKey: "MockSecretKey", Region: "us-west-2", UserAgentProducts: []*UserAgentProduct{{}}, StsEndpoint: ts.URL}, "222222222222", "aws"},
+		{"SkipCredsValidation_Config", &Config{AccessKey: "MockAccessKey", SecretKey: "MockSecretKey", Region: "us-west-2", SkipCredsValidation: true, UserAgentProducts: []*UserAgentProduct{{}}, StsEndpoint: ts.URL}, "222222222222", "aws"},
+		{"SkipRequestingAccountId_Config", &Config{AccessKey: "MockAccessKey", SecretKey: "MockSecretKey", Region: "us-west-2", SkipCredsValidation: true, SkipRequestingAccountId: true, UserAgentProducts: []*UserAgentProduct{{}}, StsEndpoint: ts.URL}, "", "aws"},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.desc, func(t *testing.T) {
+			sess, acctID, part, err := GetSessionWithAccountIDAndPartition(tc.config)
+			if err != nil {
+				t.Fatalf("GetSessionWithAccountIDAndPartition(&Config{...}) should return a valid session, but got the error %s", err)
+			}
+
+			if sess == nil {
+				t.Error("GetSession(c) resulted in a nil session")
+			}
+
+			if acctID != tc.expectedAcctID {
+				t.Errorf("GetSession(c) returned an incorrect AWS account ID, expected %q but got %q", tc.expectedAcctID, acctID)
+			}
+
+			if part != tc.expectedPartition {
+				t.Errorf("GetSession(c) returned an incorrect AWS partition, expected %q but got %q", tc.expectedPartition, part)
+			}
+		})
+	}
+}
+
 func initSessionTestEnv() (oldEnv []string) {
 	oldEnv = awstesting.StashEnv()
 	os.Setenv("AWS_CONFIG_FILE", "file_not_exists")
