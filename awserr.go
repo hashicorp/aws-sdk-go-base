66c69ce8983cc0115ad8c44221671e972b882230
Kit Ewbank <Kit_Ewbank@hotmail.com>
2020-06-02 15:44:31 -0400

    Additional AWS error functions.

--- awserr.go
+++ awserr.go
@@ -7,19 +7,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws/awserr"
 )
 
-// IsAWSErr returns true if the error matches all these conditions:
-//  * err is of type awserr.Error
-//  * Error.Code() matches code
-//  * Error.Message() contains message
-func IsAWSErr(err error, code string, message string) bool {
-	var awsErr awserr.Error
-
-	if errors.As(err, &awsErr) {
-		return awsErr.Code() == code && strings.Contains(awsErr.Message(), message)
-	}
-
-	return false
-}
+var (
+	// IsAWSErr returns true if the error matches all these conditions:
+	//  * err is of type awserr.Error
+	//  * Error.Code() matches code
+	//  * Error.Message() contains message
+	IsAWSErr = IsAWSErrCodeMessageContains
+)
 
 // IsAWSErrExtended returns true if the error matches all these conditions:
 //  * err is of type awserr.Error
@@ -40,5 +34,50 @@ func IsAWSErrExtended(err error, code string, message string, origErrMessage str
 		return strings.Contains(origErr.Error(), origErrMessage)
 	}
 
+	return false
+}
+
+// IsAWSErrCode returns true if the error matches all these conditions:
+//  * err is of type awserr.Error
+//  * Error.Code() equals code
+func IsAWSErrCode(err error, code string) bool {
+	var awsErr awserr.Error
+	if errors.As(err, &awsErr) {
+		return awsErr.Code() == code
+	}
+	return false
+}
+
+// IsAWSErrCodeContains returns true if the error matches all these conditions:
+//  * err is of type awserr.Error
+//  * Error.Code() contains code
+func IsAWSErrCodeContains(err error, code string) bool {
+	var awsErr awserr.Error
+	if errors.As(err, &awsErr) {
+		return strings.Contains(awsErr.Code(), code)
+	}
+	return false
+}
+
+// IsAWSErrCodeMessageContains returns true if the error matches all these conditions:
+//  * err is of type awserr.Error
+//  * Error.Code() equals code
+//  * Error.Message() contains message
+func IsAWSErrCodeMessageContains(err error, code string, message string) bool {
+	var awsErr awserr.Error
+	if errors.As(err, &awsErr) {
+		return awsErr.Code() == code && strings.Contains(awsErr.Message(), message)
+	}
+	return false
+}
+
+// IsAWSErrRequestFailureStatusCode returns true if the error matches all these conditions:
+//  * err is of type awserr.RequestFailure
+//  * RequestFailure.StatusCode() equals statusCode
+func IsAWSErrRequestFailureStatusCode(err error, statusCode int) bool {
+	var awsErr awserr.RequestFailure
+	if errors.As(err, &awsErr) {
+		return awsErr.StatusCode() == statusCode
+	}
 	return false
 }
--- awserr_test.go
+++ awserr_test.go
@@ -408,3 +408,411 @@ func TestIsAwsErrExtended(t *testing.T) {
 		})
 	}
 }
+
+func TestIsAwsErrCode(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Err      error
+		Code     string
+		Expected bool
+	}{
+		{
+			Name: "nil error",
+			Err:  nil,
+		},
+		{
+			Name: "nil error code",
+			Err:  nil,
+			Code: "test",
+		},
+		{
+			Name: "other error",
+			Err:  errors.New("test"),
+		},
+		{
+			Name: "other error code",
+			Err:  errors.New("test"),
+			Code: "test",
+		},
+		{
+			Name:     "awserr error matching code",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name: "awserr error no code",
+			Err:  awserr.New("TestCode", "TestMessage", nil),
+		},
+		{
+			Name: "awserr error non-matching code",
+			Err:  awserr.New("TestCode", "TestMessage", nil),
+			Code: "NotMatching",
+		},
+		{
+			Name: "wrapped other error",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+		},
+		{
+			Name: "wrapped other error code",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+			Code: "test",
+		},
+		{
+			Name:     "wrapped awserr error matching code",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name: "wrapped awserr error no code",
+			Err:  fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+		},
+		{
+			Name: "wrapped awserr error non-matching code",
+			Err:  fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code: "NotMatching",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			got := IsAWSErrCode(testCase.Err, testCase.Code)
+
+			if got != testCase.Expected {
+				t.Errorf("got %t, expected %t", got, testCase.Expected)
+			}
+		})
+	}
+}
+
+func TestIsAwsErrCodeContains(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Err      error
+		Code     string
+		Expected bool
+	}{
+		{
+			Name: "nil error",
+			Err:  nil,
+		},
+		{
+			Name: "nil error code",
+			Err:  nil,
+			Code: "test",
+		},
+		{
+			Name: "other error",
+			Err:  errors.New("test"),
+		},
+		{
+			Name: "other error code",
+			Err:  errors.New("test"),
+			Code: "test",
+		},
+		{
+			Name:     "awserr error matching code",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name:     "awserr error contains code",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "Test",
+			Expected: true,
+		},
+		{
+			Name:     "awserr error no code",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Expected: true,
+		},
+		{
+			Name: "awserr error non-matching code",
+			Err:  awserr.New("TestCode", "TestMessage", nil),
+			Code: "NotMatching",
+		},
+		{
+			Name: "wrapped other error",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+		},
+		{
+			Name: "wrapped other error code",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+			Code: "test",
+		},
+		{
+			Name:     "wrapped awserr error matching code",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name:     "wrapped awserr error contains code",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "Test",
+			Expected: true,
+		},
+		{
+			Name:     "wrapped awserr error no code",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Expected: true,
+		},
+		{
+			Name: "wrapped awserr error non-matching code",
+			Err:  fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code: "NotMatching",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			got := IsAWSErrCodeContains(testCase.Err, testCase.Code)
+
+			if got != testCase.Expected {
+				t.Errorf("got %t, expected %t", got, testCase.Expected)
+			}
+		})
+	}
+}
+
+func TestIsAWSErrCodeMessageContains(t *testing.T) {
+	testCases := []struct {
+		Name     string
+		Err      error
+		Code     string
+		Message  string
+		Expected bool
+	}{
+		{
+			Name: "nil error",
+			Err:  nil,
+		},
+		{
+			Name: "nil error code",
+			Err:  nil,
+			Code: "test",
+		},
+		{
+			Name:    "nil error message",
+			Err:     nil,
+			Message: "test",
+		},
+		{
+			Name:    "nil error code and message",
+			Err:     nil,
+			Code:    "test",
+			Message: "test",
+		},
+		{
+			Name: "other error",
+			Err:  errors.New("test"),
+		},
+		{
+			Name: "other error code",
+			Err:  errors.New("test"),
+			Code: "test",
+		},
+		{
+			Name:    "other error message",
+			Err:     errors.New("test"),
+			Message: "test",
+		},
+		{
+			Name:    "other error code and message",
+			Err:     errors.New("test"),
+			Code:    "test",
+			Message: "test",
+		},
+		{
+			Name:     "awserr error matching code and no message",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name:     "awserr error matching code and matching message exact",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "TestCode",
+			Message:  "TestMessage",
+			Expected: true,
+		},
+		{
+			Name:     "awserr error matching code and matching message contains",
+			Err:      awserr.New("TestCode", "TestMessage", nil),
+			Code:     "TestCode",
+			Message:  "Message",
+			Expected: true,
+		},
+		{
+			Name:    "awserr error matching code and non-matching message",
+			Err:     awserr.New("TestCode", "TestMessage", nil),
+			Code:    "TestCode",
+			Message: "NotMatching",
+		},
+		{
+			Name: "awserr error no code",
+			Err:  awserr.New("TestCode", "TestMessage", nil),
+		},
+		{
+			Name:    "awserr error no code and matching message exact",
+			Err:     awserr.New("TestCode", "TestMessage", nil),
+			Message: "TestMessage",
+		},
+		{
+			Name: "awserr error non-matching code",
+			Err:  awserr.New("TestCode", "TestMessage", nil),
+			Code: "NotMatching",
+		},
+		{
+			Name:    "awserr error non-matching code and message exact",
+			Err:     awserr.New("TestCode", "TestMessage", nil),
+			Message: "TestMessage",
+		},
+		{
+			Name: "wrapped other error",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+		},
+		{
+			Name: "wrapped other error code",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+			Code: "test",
+		},
+		{
+			Name:    "wrapped other error message",
+			Err:     fmt.Errorf("test: %w", errors.New("test")),
+			Message: "test",
+		},
+		{
+			Name:    "wrapped other error code and message",
+			Err:     fmt.Errorf("test: %w", errors.New("test")),
+			Code:    "test",
+			Message: "test",
+		},
+		{
+			Name:     "wrapped awserr error matching code and no message",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "TestCode",
+			Expected: true,
+		},
+		{
+			Name:     "wrapped awserr error matching code and matching message exact",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "TestCode",
+			Message:  "TestMessage",
+			Expected: true,
+		},
+		{
+			Name:     "wrapped awserr error matching code and matching message contains",
+			Err:      fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:     "TestCode",
+			Message:  "Message",
+			Expected: true,
+		},
+		{
+			Name:    "wrapped awserr error matching code and non-matching message",
+			Err:     fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code:    "TestCode",
+			Message: "NotMatching",
+		},
+		{
+			Name: "wrapped awserr error no code",
+			Err:  fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+		},
+		{
+			Name:    "wrapped awserr error no code and matching message exact",
+			Err:     fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Message: "TestMessage",
+		},
+		{
+			Name: "wrapped awserr error non-matching code",
+			Err:  fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Code: "NotMatching",
+		},
+		{
+			Name:    "wrapped awserr error non-matching code and message exact",
+			Err:     fmt.Errorf("test: %w", awserr.New("TestCode", "TestMessage", nil)),
+			Message: "TestMessage",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			got := IsAWSErrCodeMessageContains(testCase.Err, testCase.Code, testCase.Message)
+
+			if got != testCase.Expected {
+				t.Errorf("got %t, expected %t", got, testCase.Expected)
+			}
+		})
+	}
+}
+
+func TestIsAWSErrRequestFailureStatusCode(t *testing.T) {
+	testCases := []struct {
+		Name       string
+		Err        error
+		StatusCode int
+		Expected   bool
+	}{
+		{
+			Name: "nil error",
+			Err:  nil,
+		},
+		{
+			Name:       "nil error status code",
+			Err:        nil,
+			StatusCode: 42,
+		},
+		{
+			Name: "other error",
+			Err:  errors.New("test"),
+		},
+		{
+			Name:       "other error status code",
+			Err:        errors.New("test"),
+			StatusCode: 42,
+		},
+		{
+			Name:       "awserr error matching status code",
+			Err:        awserr.NewRequestFailure(awserr.New("TestCode", "TestMessage", nil), 42, ""),
+			StatusCode: 42,
+			Expected:   true,
+		},
+		{
+			Name:       "awserr error non-matching statuc code",
+			Err:        awserr.NewRequestFailure(awserr.New("TestCode", "TestMessage", nil), 404, ""),
+			StatusCode: 42,
+		},
+		{
+			Name: "wrapped other error",
+			Err:  fmt.Errorf("test: %w", errors.New("test")),
+		},
+		{
+			Name:       "wrapped other status code",
+			Err:        fmt.Errorf("test: %w", errors.New("test")),
+			StatusCode: 42,
+		},
+		{
+			Name:       "wrapped awserr error matching status code",
+			Err:        fmt.Errorf("test: %w", awserr.NewRequestFailure(awserr.New("TestCode", "TestMessage", nil), 42, "")),
+			StatusCode: 42,
+			Expected:   true,
+		},
+		{
+			Name:       "wrapped awserr error non-matching status code",
+			Err:        fmt.Errorf("test: %w", awserr.NewRequestFailure(awserr.New("TestCode", "TestMessage", nil), 404, "")),
+			StatusCode: 42,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			got := IsAWSErrRequestFailureStatusCode(testCase.Err, testCase.StatusCode)
+
+			if got != testCase.Expected {
+				t.Errorf("got %t, expected %t", got, testCase.Expected)
+			}
+		})
+	}
+}
