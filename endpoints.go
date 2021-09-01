7fcc5128c336c57e1e5c47b31e4f7c7297515e1a
Graham Davison <gdavison@hashicorp.com>
2021-09-01 16:54:23 -0700

    Cleanup

--- endpoints.go
+++ endpoints.go
@@ -4,17 +4,22 @@ import (
 	"log"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/iam"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
 )
 
 func endpointResolver(c *Config) aws.EndpointResolver {
 	resolver := func(service, region string) (aws.Endpoint, error) {
+		log.Printf("[DEBUG] Resolving endpoint for %q in %q", service, region)
 		switch service {
-		// case iam.ServiceName:
-		// 	if endpoint := c.IamEndpoint; endpoint != "" {
-		// 		log.Printf("[INFO] Setting custom IAM endpoint: %s", endpoint)
-		// 		resolvedEndpoint.URL = endpoint
-		// 	}
+		case iam.ServiceID:
+			if endpoint := c.IamEndpoint; endpoint != "" {
+				log.Printf("[INFO] Setting custom IAM endpoint: %s", endpoint)
+				return aws.Endpoint{
+					URL:    endpoint,
+					Source: aws.EndpointSourceCustom,
+				}, nil
+			}
 		case sts.ServiceID:
 			if endpoint := c.StsEndpoint; endpoint != "" {
 				log.Printf("[INFO] Setting custom STS endpoint: %s", endpoint)
--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/aws/aws-sdk-go-v2/config v1.7.0
 	github.com/aws/aws-sdk-go-v2/credentials v1.4.0
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.5.0
+	github.com/aws/aws-sdk-go-v2/service/iam v1.9.0
 	github.com/aws/aws-sdk-go-v2/service/sts v1.7.0
 	github.com/aws/smithy-go v1.8.0
 	github.com/google/go-cmp v0.5.6
 	github.com/hashicorp/go-cleanhttp v0.5.2
 	github.com/hashicorp/go-multierror v1.1.1
-	github.com/mitchellh/go-homedir v1.1.0
 )
 
 go 1.16
