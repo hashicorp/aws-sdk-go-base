7c77742b6ff6ca505422c2b2e5a8a85dfbe25a67
Graham Davison <gdavison@hashicorp.com>
2022-02-16 14:37:11 -0800

    Centralizes HTTP client configuration tests

--- http_client_test.go
+++ http_client_test.go
@@ -1,11 +1,10 @@
 package awsbase
 
 import (
-	"crypto/tls"
 	"testing"
 
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/hashicorp/aws-sdk-go-base/v2/internal/config"
+	"github.com/hashicorp/aws-sdk-go-base/v2/internal/test"
 )
 
 func TestHTTPClientConfiguration_basic(t *testing.T) {
@@ -16,35 +15,7 @@ func TestHTTPClientConfiguration_basic(t *testing.T) {
 
 	transport := client.GetTransport()
 
-	if a, e := transport.MaxIdleConns, awshttp.DefaultHTTPTransportMaxIdleConns; a != e {
-		t.Errorf("expected MaxIdleConns to be %d, got %d", e, a)
-	}
-	if a, e := transport.MaxIdleConnsPerHost, awshttp.DefaultHTTPTransportMaxIdleConnsPerHost; a != e {
-		t.Errorf("expected MaxIdleConnsPerHost to be %d, got %d", e, a)
-	}
-	if a, e := transport.IdleConnTimeout, awshttp.DefaultHTTPTransportIdleConnTimeout; a != e {
-		t.Errorf("expected IdleConnTimeout to be %s, got %s", e, a)
-	}
-	if a, e := transport.TLSHandshakeTimeout, awshttp.DefaultHTTPTransportTLSHandleshakeTimeout; a != e {
-		t.Errorf("expected TLSHandshakeTimeout to be %s, got %s", e, a)
-	}
-	if a, e := transport.ExpectContinueTimeout, awshttp.DefaultHTTPTransportExpectContinueTimeout; a != e {
-		t.Errorf("expected ExpectContinueTimeout to be %s, got %s", e, a)
-	}
-	if !transport.ForceAttemptHTTP2 {
-		t.Error("expected ForceAttemptHTTP2 to be true, got false")
-	}
-	if transport.DisableKeepAlives {
-		t.Error("expected DisableKeepAlives to be false, got true")
-	}
-
-	tlsConfig := transport.TLSClientConfig
-	if a, e := int(tlsConfig.MinVersion), tls.VersionTLS12; a != e {
-		t.Errorf("expected tlsConfig.MinVersion to be %d, got %d", e, a)
-	}
-	if tlsConfig.InsecureSkipVerify {
-		t.Error("expected InsecureSkipVerify to be false, got true")
-	}
+	test.HTTPClientConfigurationTest_basic(t, transport)
 }
 
 func TestHTTPClientConfiguration_insecureHTTPS(t *testing.T) {
@@ -57,8 +28,5 @@ func TestHTTPClientConfiguration_insecureHTTPS(t *testing.T) {
 
 	transport := client.GetTransport()
 
-	tlsConfig := transport.TLSClientConfig
-	if !tlsConfig.InsecureSkipVerify {
-		t.Error("expected InsecureSkipVerify to be true, got false")
-	}
+	test.HTTPClientConfigurationTest_insecureHTTPS(t, transport)
 }
--- v2/awsv1shim/http_client_test.go
+++ v2/awsv1shim/http_client_test.go
@@ -1,12 +1,11 @@
 package awsv1shim
 
 import (
-	"crypto/tls"
 	"net/http"
 	"testing"
 
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 	"github.com/hashicorp/aws-sdk-go-base/v2/internal/config"
+	"github.com/hashicorp/aws-sdk-go-base/v2/internal/test"
 )
 
 func TestHTTPClientConfiguration_basic(t *testing.T) {
@@ -20,35 +19,7 @@ func TestHTTPClientConfiguration_basic(t *testing.T) {
 		t.Fatalf("Unexpected type for HTTP client transport: %T", client.Transport)
 	}
 
-	if a, e := transport.MaxIdleConns, awshttp.DefaultHTTPTransportMaxIdleConns; a != e {
-		t.Errorf("expected MaxIdleConns to be %d, got %d", e, a)
-	}
-	if a, e := transport.MaxIdleConnsPerHost, awshttp.DefaultHTTPTransportMaxIdleConnsPerHost; a != e {
-		t.Errorf("expected MaxIdleConnsPerHost to be %d, got %d", e, a)
-	}
-	if a, e := transport.IdleConnTimeout, awshttp.DefaultHTTPTransportIdleConnTimeout; a != e {
-		t.Errorf("expected IdleConnTimeout to be %s, got %s", e, a)
-	}
-	if a, e := transport.TLSHandshakeTimeout, awshttp.DefaultHTTPTransportTLSHandleshakeTimeout; a != e {
-		t.Errorf("expected TLSHandshakeTimeout to be %s, got %s", e, a)
-	}
-	if a, e := transport.ExpectContinueTimeout, awshttp.DefaultHTTPTransportExpectContinueTimeout; a != e {
-		t.Errorf("expected ExpectContinueTimeout to be %s, got %s", e, a)
-	}
-	if !transport.ForceAttemptHTTP2 {
-		t.Error("expected ForceAttemptHTTP2 to be true, got false")
-	}
-	if transport.DisableKeepAlives {
-		t.Error("expected DisableKeepAlives to be false, got true")
-	}
-
-	tlsConfig := transport.TLSClientConfig
-	if a, e := int(tlsConfig.MinVersion), tls.VersionTLS12; a != e {
-		t.Errorf("expected tlsConfig.MinVersion to be %d, got %d", e, a)
-	}
-	if tlsConfig.InsecureSkipVerify {
-		t.Error("expected InsecureSkipVerify to be false, got true")
-	}
+	test.HTTPClientConfigurationTest_basic(t, transport)
 }
 
 func TestHTTPClientConfiguration_insecureHTTPS(t *testing.T) {
@@ -64,8 +35,5 @@ func TestHTTPClientConfiguration_insecureHTTPS(t *testing.T) {
 		t.Fatalf("Unexpected type for HTTP client transport: %T", client.Transport)
 	}
 
-	tlsConfig := transport.TLSClientConfig
-	if !tlsConfig.InsecureSkipVerify {
-		t.Error("expected InsecureSkipVerify to be true, got false")
-	}
+	test.HTTPClientConfigurationTest_insecureHTTPS(t, transport)
 }
