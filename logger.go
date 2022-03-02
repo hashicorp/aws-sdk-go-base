5f9acd9143e78dd1698fe1fcc8ea76964d2d7b63
Graham Davison <gdavison@hashicorp.com>
2022-03-02 12:06:35 -0800

    Filters out CR (`\r`) characters from log messages

--- logger.go
+++ logger.go
@@ -3,6 +3,7 @@ package awsbase
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/smithy-go/logging"
 )
@@ -10,5 +11,7 @@ import (
 type debugLogger struct{}
 
 func (l debugLogger) Logf(classification logging.Classification, format string, v ...interface{}) {
-	log.Printf("[%s] [aws-sdk-go-v2] %s", classification, fmt.Sprintf(format, v...))
+	s := fmt.Sprintf(format, v...)
+	s = strings.ReplaceAll(s, "\r", "") // Works around https://github.com/jen20/teamcity-go-test/pull/2
+	log.Printf("[%s] [aws-sdk-go-v2] %s", classification, s)
 }
