f713d29bc63b611512209ea5360c7db99eaa92e6
Adrian Johnson <adrian.johnson@hashicorp.com>
2023-06-22 13:20:35 -0500

    add test for masking sensitive values

---
+++ logging/aws_test.go
@@ -0,0 +1,66 @@
+package logging
+
+import (
+	"testing"
+)
+
+func TestMaskAWSSensitiveValues(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		input    string
+		expected string
+	}
+
+	tests := map[string]testCase{
+		"mask_simple": {
+			input:    "4skd4lTSLVBMG/asedterGLKSNMSAlsxiLGfjt=ssD",
+			expected: "4skd**********************************=ssD",
+		},
+		"mask_complex_json": {
+			input: `
+{
+	"AWSSecretKey": "4skd4lTSLVBMG/asedterGLKSNMSAlsxiLGfjt=ssD",
+	"BucketName": "test-bucket",
+	"AWSKeyId": "AIDACKCEVSQ6C2EXAMPLE",
+}
+`,
+			expected: `
+{
+	"AWSSecretKey": "4skd**********************************=ssD",
+	"BucketName": "test-bucket",
+	"AWSKeyId": "AIDA*************MPLE",
+}
+`,
+		},
+		"no_mask": {
+			input:    "<BucketName>test-bucket</BucketName>",
+			expected: "<BucketName>test-bucket</BucketName>",
+		},
+		"mask_xml": {
+			input: `
+<AWSSecretKey>4skd4lTSLVBMG/asedterGLKSNMSAlsxiLGfjt=ssD</AWSSecretKey>
+<BucketName>test-bucket</BucketName>
+<AWSKeyId>AIDACKCEVSQ6C2EXAMPLE</AWSKeyId>
+`,
+			expected: `
+<AWSSecretKey>4skd**********************************=ssD</AWSSecretKey>
+<BucketName>test-bucket</BucketName>
+<AWSKeyId>AIDA*************MPLE</AWSKeyId>
+`,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			got := MaskAWSSensitiveValues(test.input)
+
+			if got != test.expected {
+				t.Errorf("unexpected diff +wanted: %s, -got: %s", test.expected, got)
+			}
+		})
+	}
+}
